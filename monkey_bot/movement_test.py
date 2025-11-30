1e15d54079018a575689a6c7528647115b3b9dcc
Parsa Sedighi <parsasedighi2004@icloud.com>
2025-11-29 22:49:51 -0800

    Added computer Vision logic

--- monkey_bot/movement_test.py
+++ monkey_bot/movement_test.py
@@ -14,9 +14,11 @@ PORT = "/dev/ttyACM0"   # Common on Linux/RPi; change if needed
 BAUDRATE = 9600
 TIMEOUT = 1.0           # seconds for read timeout
 
-
+# Class for interfacing between arduino and raspberry pi
 class MotorSerialClient:
+    # Initializing serial ports
     def __init__(self, port=PORT, baudrate=BAUDRATE, timeout=TIMEOUT):
+        # Establish the serial port to the Arduino Uno, ensuring baudrates and port is correct
         self.ser = serial.Serial(port, baudrate=baudrate, timeout=timeout)
         # Give Arduino a moment to reset after opening serial
         time.sleep(2.0)
@@ -25,6 +27,7 @@ class MotorSerialClient:
         self.ser.reset_input_buffer()
         self.ser.reset_output_buffer()
 
+    # Format the PWM values correctly for Arduino to read once it is sent/written on serial
     def set_motors(self, m1, m2):
         """
         Set both motors.
@@ -34,11 +37,14 @@ class MotorSerialClient:
         # Clamp just in case
         m1 = max(-255, min(255, int(m1)))
         m2 = max(-255, min(255, int(m2)))
-
+        # Format the lines
         line = f"{m1},{m2}\n"
+        # Write into serial port using the ascii characters
         self.ser.write(line.encode("ascii"))
+        # To ensure that all data currently in the serial output buffer is sent to the connected device, blocking until all bytes are written:
         self.ser.flush()
 
+    # Read from the Arduino 
     def read_line(self):
         """
         Read one line of feedback from Arduino (non-blocking-ish).
@@ -46,11 +52,10 @@ class MotorSerialClient:
         """
         line = self.ser.readline().decode(errors="ignore").strip()
         return line if line else None
-
+    # Close the serial port
     def close(self):
         self.ser.close()
-
-
+# Hardcoded, predetermined movements
 def simple_test_pattern():
     """
     Demo pattern:
@@ -87,7 +92,7 @@ def simple_test_pattern():
         client.close()
         print("Disconnected.")
 
-
+# Interactive 
 def interactive_cli():
     """
     Simple interactive terminal:
